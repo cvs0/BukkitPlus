384cb05b477c62ab2af04ea20af86801db675e3c
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-16 10:24:16 -0600

    Initial commit

---
+++ src/main/java/com/cvs0/bukkitplus/BukkitPlus.java
@@ -0,0 +1,17 @@
+package com.cvs0.bukkitplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BukkitPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
