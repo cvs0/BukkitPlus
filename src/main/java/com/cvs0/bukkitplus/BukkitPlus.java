a0f7cd9ff3fea896cb5f8b1172255b6b3e1e4fa6
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-16 10:30:39 -0600

    Added commands + cleanup

--- src/main/java/com/cvs0/bukkitplus/BukkitPlus.java
+++ src/main/java/com/cvs0/bukkitplus/BukkitPlus.java
@@ -1,17 +1,57 @@
 package com.cvs0.bukkitplus;
 
+import java.util.Objects;
+import java.util.logging.Logger;
+
+import com.cvs0.bukkitplus.commands.ReloadConfigCommand;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.raid.RaidTriggerEvent;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BukkitPlus extends JavaPlugin {
+public final class BukkitPlus extends JavaPlugin implements Listener {
+    public static Logger logger;
+    private FileConfiguration config;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        logger = getLogger();
+        config = getConfig();
 
+        config.options().copyDefaults(true);
+
+        saveDefaultConfig();
+
+        Objects.requireNonNull(getCommand("reloadconfig")).setExecutor(new ReloadConfigCommand(this));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void raidTriggerEvent(RaidTriggerEvent event) {
+        String playerName = event.getPlayer().getName();
+
+        if(config.getBoolean("removeRaids")) {
+            event.setCancelled(true);
+
+            logger.info("Stopped a raid triggered by: " + playerName);
+        }
+    }
+
+    @EventHandler
+    public void spawnEvent(CreatureSpawnEvent event) {
+        Entity entity = event.getEntity();
+
+        if(entity.getType() == EntityType.PHANTOM && config.getBoolean("removePhantoms")) {
+            event.setCancelled(true);
+        }
     }
 }
---
+++ src/main/java/com/cvs0/bukkitplus/commands/ReloadConfigCommand.java
@@ -0,0 +1,25 @@
+package com.cvs0.bukkitplus.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ReloadConfigCommand implements CommandExecutor {
+
+    private final JavaPlugin plugin;
+
+    public ReloadConfigCommand(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("reloadconfig")) {
+            plugin.reloadConfig();
+            sender.sendMessage("Configuration reloaded.");
+            return true;
+        }
+        return false;
+    }
+}
