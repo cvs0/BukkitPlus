5a09395beec7dc5d700ac54636bb32923fac90eb
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-16 10:31:47 -0600

    NotNull

--- src/main/java/com/cvs0/bukkitplus/commands/ReloadConfigCommand.java
+++ src/main/java/com/cvs0/bukkitplus/commands/ReloadConfigCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public class ReloadConfigCommand implements CommandExecutor {
 
@@ -14,7 +15,7 @@ public class ReloadConfigCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("reloadconfig")) {
             plugin.reloadConfig();
             sender.sendMessage("Configuration reloaded.");
